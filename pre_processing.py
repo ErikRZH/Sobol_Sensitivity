8f32b15c87d4cc48d0f7547fb475b86d9cf107ea
Erik <supersamst.rydow@gmail.com>
2021-10-04 17:04:36 +0100

    Separated file wise data extraction into a function

--- pre_processing.py
+++ pre_processing.py
@@ -1,27 +1,37 @@
 'this class parses model runs and returns a CSV with the parameters and mean and variance of the output to be analysed'
 import csv
 import pandas as pd
+import numpy as np
 
 
-location = "output_prediction_simu_11-09-2020_18-57-41.txt"
-iter_name = "iter" #name of column which stores iterations
-quantity_name = " inc_death" #name of model quanitity to be summed
+def data_extraction_sum(location, iter_name, quantity_name):
+    #Extracts data from CVS file at "location" and then sums over the desired quantity, given by "quantity_name" for each model iteration'
+    #returns the mean and variance of the sum of the quantity
 
-df = pd.read_csv(location)
+    df = pd.read_csv(location)
 
-#get all unique values of the iterations in file
-uniqueValues = df[iter_name].unique()
+    # get all unique values of the iterations in file
+    unique_iters = df[iter_name].unique()
 
-print(uniqueValues)
+    output_array = np.zeros(len(unique_iters))
 
-# loop through iterations
-for i in uniqueValues:
-    iter_df = df.loc[df[iter_name] == i]
-    quantity_total = iter_df[quantity_name].sum()
-    print(quantity_total) #Checked the sums using excel
+    # loop through iterations and sum the deaths
+    for i in unique_iters:
+        iter_df = df.loc[df[iter_name] == i]
+        quantity_total = iter_df[quantity_name].sum()  # Checked the sums using excel
+        output_array[i] = quantity_total
+
+    output_mean = output_array.mean()
+    output_variance = output_array.var()
+
+    return output_mean,output_variance
 
 
-print(df.head(3))
-print(type(df))
+my_location = "output_prediction_simu_11-09-2020_18-57-41.txt"
+my_iter_name = "iter" #name of column which stores iterations
+my_quantity_name = " inc_death" #name of model quanitity to be summed
 
+deaths_mean, deaths_variance = data_extraction_sum(my_location,my_iter_name,my_quantity_name)
+
+print("mean deaths for this parameter choice is ", deaths_mean, " the varaince is ", deaths_variance)
 
