8972cf047ac8fad42119e2950e3ce3d5d1841eee
Erik <supersamst.rydow@gmail.com>
2021-10-05 13:24:55 +0100

    Extracting parameters and output to CSV

--- pre_processing.py
+++ pre_processing.py
@@ -8,8 +8,8 @@ import numpy as np
 
 def data_extraction_sum(location, iter_name, quantity_name):
     '''
-    Extracts data from CVS file at "location" and then sums over the desired quantity, given by "quantity_name" for each model iteration'
-    returns: the mean and variance of the sum of the quantity
+    Extracts data from CSV file at "location" and then sums over the desired quantity, given by "quantity_name" for each model iteration.
+    returns: the mean and variance of the sum of the quantity.
     '''
 
     df = pd.read_csv(location)
@@ -33,9 +33,10 @@ def data_extraction_sum(location, iter_name, quantity_name):
 
 
 def get_index_locations():
-    '''heavily data structure dependent function.
-    Gives id and associated location of all runs as a dictionary
-    returns: dictionary with key=run_index,value=run_location
+    '''
+    Heavily data structure dependent function.
+    Gives id and associated location of all runs as a dictionary.
+    returns: dictionary with key=run_index,value=run_location.
     '''
 
     # nr of different folders (1,2,3,4)
@@ -75,16 +76,24 @@ def get_index_locations():
 
     return output_index_location
 
+
 index_locations = get_index_locations()
 
-print(index_locations)
-
-my_location = index_locations[0]
 my_iter_name = "iter"  # name of column which stores iterations
 my_quantity_name = " inc_death"  # name of model quanitity to be summed
 
-deaths_mean, deaths_variance = data_extraction_sum(my_location, my_iter_name, my_quantity_name)
+'''creating master CSV file'''
+quantity_mean = "total_deaths_mean"
+quantity_variance = "total_deaths_variance"
+df = pd.read_csv("posterior_parameters.csv")
+df.set_index("Index")
+df[quantity_mean] = np.nan
+df[quantity_variance] = np.nan
 
-print("mean deaths for this parameter choice is ", deaths_mean, " the varaince is ", deaths_variance)
+for i in df["Index"]:
+    i_deaths_mean, i_deaths_variance = data_extraction_sum(index_locations[i], my_iter_name, my_quantity_name)
+    df.at[i, quantity_mean] = i_deaths_mean
+    df.at[i, quantity_variance] = i_deaths_variance
+    print(i)
 
-
+df.to_csv("parameter_output.csv", index=False)
