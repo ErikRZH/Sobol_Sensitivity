dddc6171c349e7ebd8869147fdab167e1a3a86f9
Erik <supersamst.rydow@gmail.com>
2021-10-08 12:32:28 +0100

    Added Comments and Doc Strings

--- sensitivity_analysis_sobol.py
+++ sensitivity_analysis_sobol.py
@@ -3,18 +3,25 @@ from SALib.sample import saltelli
 import gaussian_process_emulator as gpe
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from typing import Tuple
 
 
-def salteli_with_constant_bounds(problem_in, N_in):
-    """
-    Takes as input a problem dictionary, which may include constants(!), and an integer N
-    provides as output a saletli sampling of the parameters, while ignoring the constants, Returned as a (D,N*(2D+2)) numpy array
-    Also outputs the problem dictionary with constants removed
+def salteli_with_constant_bounds(problem_in: dict, N_in: int) -> Tuple[np.ndarray, dict]:
     """
 
-    # saletlli_sample_with_constants
-    '''removes the constant parameters from the problem dictionary and then generates samples and reinserts constant values'''
+    Takes as input a problem dictionary, which may include constants(!), and an integer N.
+    Returns a Saletli sampling of the parameters, ignoring the constants, as a (D,N*(2D+2)) numpy array,
+    and the problem dictionary with constants removed
+
+    Arg:
+        problem_in: Problem dictionary (keys: 'num_vars', 'names', 'bounds') which may contain constants.
+        N_in: N used to determine number of parameter samples in Salteli sampling.  Note: Must be power of 2.
+
+    Returns:
+        X_sample: Parameter values to be sampled in Sobol sensitivity analysis.
+    """
+
+    # Removes the constant parameters from the problem dictionary, generates samples and reinserts constant values
     constant_parameters = []
     for count, value in enumerate(problem_in['bounds']):
         if len(value) == 1:
@@ -32,30 +39,37 @@ def salteli_with_constant_bounds(problem_in, N_in):
                          'names': names_variables,
                          'bounds': bounds_variables}
 
-    '''generates N*(2D+2) samples, where N is argument'''
+    # Generates N*(2D+2) samples, where N is argument D is number of non-constant varaibles
     X_sample = saltelli.sample(problem_variables, N_in)
 
     # Reinsert Constant Values
     for i in constant_parameters:
-        # Inserts column with the constant value in correct place
+        # Inserts columns with the constant value in correct place
         X_sample = np.insert(X_sample, i, np.ones([X_sample.shape[0]]) * problem_in['bounds'][i][0], axis=1)
 
     return X_sample, problem_variables
 
-def sobol_indicies_from_emulator(df_in, params_in,bounds_in,quantity_mean_in, quantity_variance_in,N_in):
+
+def sobol_indicies_from_emulator(df_in: pd.DataFrame, params_in: list, bounds_in: list, quantity_mean_in: str,
+                                 quantity_variance_in: str, N_in: int) -> pd.DataFrame:
     """
 
-    Computes Sobol indicies from a pandas dataframe using a gaussian emulator
-        Args:
-            df_in: dataframe with columns being parameters,output mean,output variance. each row is a different sample
-            params_in: names of model parameters (can include constants)
-            bounds_in: the bounds on the model parameters as [[param1_lower_bound,param1_upper_bound],[...],[param_x_constant],..]. For fixed values of parameters this is the fixed value
-            quantity_mean_in: the name of the column containing the mean of the output quantity
-            quantity_variance_in: name of the column containing the variance of the output quantity
-            N_in: (POWER OF 2!) the number used in parameter sampling (N*(2D+2) samples are generated, D number of non-constant parameters)
+    Returns Sobol indices by sampling from a gaussian process emulator trained on model data.
 
-        Returns:
-            df_out: pandas dataframe with its columns being the 1st and total order sensitivity indicies, and their associated 95% confidence intervals. The rows are the non-constant parameters
+    Args:
+        df_in: Dataframe of model data, each row is a different sample.
+            Columns are: parameters, output mean, output variance.
+        params_in: Names of model parameters (can include constants).
+        bounds_in: Bounds of model parameters as [[param1_lower_bound,param1_upper_bound],[...],[param_x_fixed],..].
+            For parameters with a fixed value this is the fixed value.
+        quantity_mean_in: Name of the column containing the mean of the output quantity.
+        quantity_variance_in: Name of the column containing the variance of the output quantity.
+        N_in: The number used in parameter sampling, should be a power of 2.
+            N*(2D+2) samples are generated where D is the number of non-constant parameters.
+
+    Returns:
+        df_out: Dataframe containing 1st and total order sensitivity indicies,
+            and associated 95% confidence intervals.
     """
     # Set up problem dictionary,as needed by SALib
     problem_all = {'num_vars': len(params_in),
@@ -71,47 +85,8 @@ def sobol_indicies_from_emulator(df_in, params_in,bounds_in,quantity_mean_in, qu
     # Input thereduced problem dictionary, the one with constants removed
     Si = sobol.analyze(problem_reduced, y)
 
-    #make into pandas dataframe
+    # make into pandas dataframe
     total_Si, first_Si, second_Si = Si.to_df()
 
-    df_out = pd.concat([first_Si,total_Si], axis=1)
+    df_out = pd.concat([first_Si, total_Si], axis=1)
     return df_out
-
-# ONLY SPECIFY ONCE
-def example():
-    df = pd.read_csv("parameters_output.csv")
-
-    parameters = ["p_inf", "p_hcw", "c_hcw", "d", "q", "p_s", "rrd", "lambda", "T_lat", "juvp_s", "T_inf", "T_rec", "T_sym",
-                  "T_hos",
-                  "K", "inf_asym"]
-
-    # a single value means it is a constant with that value
-    bounds = [[0, 1], [0, 1], [1, 80], [0, 1], [0, 1], [0, 1], [1], [1e-9, 1e-3], [0.1, 14], [0, 1], [0.1, 21], [1, 28],
-              [0.1, 14], [1, 35], [10000], [0, 1]]
-
-    quantity_mean = "total_deaths_mean"
-    quantity_varaince = "total_deaths_variance"
-
-    N = 2 ** 11  # SELECT A POWER OF 2
-
-    S1_ST_df = sobol_indicies_from_emulator(df,parameters,bounds,quantity_mean,quantity_varaince,N)
-
-    #Conf is the 95% confidence interval
-
-    # Analyse and visualise the results
-    labels = S1_ST_df.index.values
-    S1_plot = S1_ST_df ["S1"]
-    x = np.arange(len(labels))  # the label locations
-    width = 0.45  # the width of the bars
-
-    fig, ax = plt.subplots(figsize=(12, 6))
-    rects1 = ax.bar(x, S1_plot, width)
-
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('S1')
-    ax.set_title('S1 index for parameters')
-    ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.bar_label(rects1, padding=3)
-    fig.tight_layout()
-    plt.show()
